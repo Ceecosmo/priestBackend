3ec31fb9709af0e15fbc786dbcd4df28df49f555
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-27 03:57:35 +0100

    Update urls.py

--- priestsite/urls.py
+++ priestsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-admin.site.site_header="Welcome to Priests' Welfare Admin Dashboard"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
