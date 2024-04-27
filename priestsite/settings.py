754decc629d9c99bd79fa018902109269b025d6f
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-27 22:59:13 +0100

    Update settings.py

--- priestsite/settings.py
+++ priestsite/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3*4jkwro-(2930$vg5n7f)&1c_rh57bpcur@7!o)*##+t@vkb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# django-insecure-m3*4jkwro-(2930$vg5n7f)&1c_rh57bpcur@7!o)*##+t@vkb
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -90,6 +92,9 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
